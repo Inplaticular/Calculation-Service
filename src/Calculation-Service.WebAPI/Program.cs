0d015b257348231f7b0c97bd016d7ce6356fbc6e
Yiran001 <korchflorian@gmail.com>
2022-01-18 17:21:55 +0100

    added clean architecture projects

---
+++ src/Calculation-Service.WebAPI/Program.cs
@@ -0,0 +1,41 @@
+namespace Inplanticular.CalculationService.WebAPI;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        ConfigureServices(builder);
+        var app = builder.Build();
+        ConfigurePipeline(app);
+        app.Run();
+    }
+
+    private static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+    
+    private static void ConfigurePipeline(WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+    }
+    
+}
